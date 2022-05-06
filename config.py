106bb5a04d5949060974fdf70dc329dde44c9b80
Chathush Nethmal Priyankara <102727399+chathush999@users.noreply.github.com>
2022-05-06 15:24:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = getenv("API_HASH")
 ## Get it from @Botfather in Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 
-# API By TechZBots || https://t.me/TechZBots
+# API By TechZBots || https://t.me/devid_update
 LOGO_API_URL1 = "https://techzbotsapi.herokuapp.com/logo?text="
 
 LOGO_API_URL2 = "https://techzbotsapi.herokuapp.com/logo?square=true&text="
